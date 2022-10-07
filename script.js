09e6e5a1fa6071de744fac7ff9b3c260c57e77c3
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2022-10-07 22:40:00 +0200

    formatted code

--- script.js
+++ script.js
@@ -6,17 +6,16 @@ function setDate() {
   const now = new Date();
 
   const seconds = now.getSeconds();
-  const secondsDegree = ((seconds / 60) * 360) + 90;
+  const secondsDegree = (seconds / 60) * 360 + 90;
   secondHand.style.transform = `rotate(${secondsDegree}deg)`;
 
   const mins = now.getMinutes();
-  const minsDegree = ((mins / 60) * 360) + 90;
-  minsHand.style.transform = `rotate(${minsDegree}deg)`; 
+  const minsDegree = (mins / 60) * 360 + 90;
+  minsHand.style.transform = `rotate(${minsDegree}deg)`;
 
   const hours = now.getHours();
-  const hoursDegree = ((hours / 12) * 360) + 90;
-  hourHand.style.transform = `rotate(${hoursDegree}deg)`
+  const hoursDegree = (hours / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hoursDegree}deg)`;
 }
 
 setInterval(setDate, 1000);
- 
